1743893a2ddf9f697d1dc068c5d774a2a5447cf4
SeemaDubey01 <64755176+SeemaDubey01@users.noreply.github.com>
2021-10-13 00:42:00 +0200

    config changes

--- src/main/java/com/adevinta/poc/AdevintaPocApplication.java
+++ src/main/java/com/adevinta/poc/AdevintaPocApplication.java
@@ -2,12 +2,20 @@ package com.adevinta.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdevintaPocApplication {
+public class AdevintaPocApplication  extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder  configure(SpringApplicationBuilder application) {
+         return application.sources(AdevintaPocApplication .class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdevintaPocApplication.class, args);
 	}
 
 }
+
